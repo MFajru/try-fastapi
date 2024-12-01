af4addd060852f83c9ffa4395a37b1774ddc4304
MFajru <fajru234@gmail.com>
2024-12-01 13:35:33 +0700

    refactor: delete some code

--- routers/book_router.py
+++ routers/book_router.py
@@ -8,10 +8,6 @@ from services.book_service import create_author
 
 router = APIRouter()
 
-@router.get("/{id}")
-def get_book(id: int):
-    return {"data": {"name": "book"}}
-
 @router.post("/add-author", response_model=Author)
 def post_author(req: AuthorRequest,  session:Session = Depends(get_session)):
     new_author = Author(**req.model_dump())         # same as: (name = req.name, id = req.id) etc
